38ea69e21bac4a46e4198d22e833f804131914c7
ASK <placeholder@email.com>
2019-04-29 18:41:43 -0700

    trying to get these damn files to cache

--- CIS 128/Homework 4/Part 2/service-worker.js
+++ CIS 128/Homework 4/Part 2/service-worker.js
@@ -1,10 +1,10 @@
 var CACHE_VERSION = 'app-v6';
 var CACHE_FILES = [
     '/',
-    'images/lightblue.jpg',
-    'images/lightgold.jpg',
-    'app.js',
-    'assets/css/'
+    '/images/lightblue.jpg',
+    '/images/lightgold.jpg',
+    '/app.js',
+    '/assets/css/'
 ];
 
 self.addEventListener('install', event => {
